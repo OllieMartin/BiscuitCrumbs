f80d61b2a13c1c678ace39c512d13dfb4c424d39
olive <olivermartin410@yahoo.co.uk>
2016-11-08 10:31:22 +0000

    More work on executor

--- BiscuitCrumbs/src/interpreter/Executor.java
+++ BiscuitCrumbs/src/interpreter/Executor.java
@@ -6,13 +6,34 @@ import java.util.HashMap;
 public class Executor {
 	
 	protected HashMap<String,Variable> varList = new HashMap<String,Variable>();
-	
-	ArrayList<AST> tree;
 
 	public Executor() {}
 	
-	public void execute(ArrayList<AST> programTree) {
-		tree = programTree;
+	public ArrayList<AST> toPostFix
+	
+	public int evaluateInt(ArrayList<AST> astarr) {
+		
+		int finalvalue = 0;
+		
+		
+		
+		return finalvalue;
+		
+	}
+	
+	public void execute(ArrayList<AST> tree) {
+		int pc = 0;
+		
+		while (pc <= tree.size()) {
+			
+			if (tree.get(pc).getType().equals("INT")) {
+				if (tree.get(pc).getInitialValue() != null) {
+					varList.put(tree.get(pc).getValue(), new Variable(tree.get(pc).getValue(),evaluateInt(tree.get(pc).getInitialValue())));
+				}
+			}
+			
+		}
+		
 	}
 	
 }
